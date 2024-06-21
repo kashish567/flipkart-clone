08d40d36a24f4f59f32841fb81d26dac85e8ab12
kashish567 <kashishbhanushali7@gmail.com>
2024-06-21 16:34:19 +0530

    changes in ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,90 @@
-import { CarouselDemo } from "@/components/CarouselDemo";
+// import { CarouselDemo } from "@/components/CarouselDemo";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import Info from "@/components/Info";
 import ProductCard from "@/components/ProductCard";
 import Slider from "@/components/Slider";
 
 export default function Home() {
+  const Products = [
+    {
+      id: 1,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Pink",
+      price: 599,
+      rating: 5,
+      image: "/product2.webp",
+    },
+    {
+      id: 2,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Blue",
+      price: 699,
+      rating: 4,
+      image: "/product2.webp",
+    },
+    {
+      id: 3,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Green",
+      price: 799,
+      rating: 3,
+      image: "/product3.webp",
+    },
+    {
+      id: 4,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Red",
+      price: 899,
+      rating: 2,
+      image: "/product4.webp",
+    },
+    {
+      id: 5,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Pink",
+      price: 599,
+      rating: 5,
+      image: "/product2.webp",
+    },
+    {
+      id: 6,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Pink",
+      price: 599,
+      rating: 5,
+      image: "/product2.webp",
+    },
+    {
+      id: 7,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Pink",
+      price: 599,
+      rating: 5,
+      image: "/product2.webp",
+    },
+    {
+      id: 8,
+      title: "Apple Watch Series 7 GPS, Aluminium Case, Pink",
+      price: 599,
+      rating: 5,
+      image: "/product2.webp",
+    },
+  ];
   return (
-      <>
+    <>
       <Header />
+      <HeroSection />
       {/* <CarouselDemo /> */}
       <Slider />
-      <ProductCard  />
+      <div className="flex py-4 px-8 flex-wrap bg-slate-100">
+        {" "}
+        {Products.map((product) => (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            rating={product.rating}
+            price={product.price}
+            image={product.image}
+          />
+        ))}
+      </div>
+      <Info />
       <Footer />
-      </>
-      
-
+    </>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
 import React from "react";
 import product2 from "../../public/product2.webp";
-import product3 from "../../public/product3.webp";
-import product4 from "../../public/product4.webp";
+// import product3 from "../../public/product3.webp";
+// import product4 from "../../public/product4.webp";
 
+interface ProductCardProps {
+  title: string;
+  price: number;
+  rating: number;
+  image: string;
+}
 
-const ProductCard = () => {
+const ProductCard: React.FC<ProductCardProps> = ({
+  title,
+  price,
+  rating,
+  image,
+}) => {
   return (
-    <div className="flex space-x-6 bg-slate-200 mt-20 mb-20">
-      <div className="w-1/3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-lg transition duration-300">
+    <div className="bg-white ">
+      <div className="w-[288px] mx-4 my-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-lg transition duration-300">
         <a href="#">
           <div className="flex justify-center p-8">
             <Image
               className="rounded-t-lg"
-              src={product2}
+              src={image}
               alt="product image"
               width={200}
               height={200}
@@ -23,7 +34,7 @@ const ProductCard = () => {
         <div className="px-5 pb-5">
           <a href="#">
             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              Apple Watch Series 7 GPS, Aluminium Case, Pink
+              {title}
             </h5>
           </a>
           <div className="flex items-center mt-2.5 mb-5">
@@ -39,107 +50,12 @@ const ProductCard = () => {
               </svg>
             </div>
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-              5.0
+              {rating}
             </span>
           </div>
           <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
-            </span>
-            <a
-              href="#"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Add to cart
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-1/3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-lg transition duration-300">
-        <a href="#">
-          <div className="flex justify-center p-8">
-            <Image
-              className="rounded-t-lg"
-              src={product4}
-              alt="product image"
-              width={200}
-              height={200}
-            />
-          </div>
-        </a>
-        <div className="px-5 pb-5">
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              Apple Watch Series 7 GPS, Aluminium Case, Pink
-            </h5>
-          </a>
-          <div className="flex items-center mt-2.5 mb-5">
-            <div className="flex items-center space-x-1 rtl:space-x-reverse">
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M11 17.27L16.18 20l-1.64-7.03L20 9.24l-7.19-.61L11 2 9.19 8.63 2 9.24l5.46 3.73L5.82 20z" />
-              </svg>
-            </div>
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-              5.0
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
-            </span>
-            <a
-              href="#"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Add to cart
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="w-1/3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-800 hover:shadow-lg transition duration-500">
-        <a href="#">
-          <div className="flex justify-center p-8">
-            <Image
-              className="rounded-t-lg"
-              src={product3}
-              alt="product image"
-              width={200}
-              height={200}
-            />
-          </div>
-        </a>
-        <div className="px-5 pb-5">
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              Apple Watch Series 5, Aluminium Case, Starlight Sport
-            </h5>
-          </a>
-          <div className="flex items-center mt-2.5 mb-5">
-            <div className="flex items-center space-x-1 rtl:space-x-reverse">
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M11 17.27L16.18 20l-1.64-7.03L20 9.24l-7.19-.61L11 2 9.19 8.63 2 9.24l5.46 3.73L5.82 20z" />
-              </svg>
-            </div>
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-              5.0
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
+              {price}
             </span>
             <a
               href="#"
