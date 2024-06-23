6a87e447507b15782de18fa1f6b81b1337d53930
kashish567 <kashishbhanushali7@gmail.com>
2024-06-23 15:33:43 +0530

    minor ui changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -161,9 +161,18 @@ const Header: React.FC<HeaderProps> = ({ count }) => {
             </ul>
           </div>
         </div>
-        <Link href="/cart" className="flex">
-          <GrCart className="h-5 w-5" /> Cart{" "}
-          <span className="font-bold mx-2">{count}</span>
+        <Link className="flex" href={"/cart"}>
+          <button
+            type="button"
+            className="relative inline-flex items-center p-3 text-sm font-medium text-center text-black"
+          >
+            <GrCart className="h-5 w-5" /> <span className="ml-2">cart</span>
+            {count > 0 && (
+              <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-1 -end-2 dark:border-gray-900">
+                {count}
+              </div>
+            )}
+          </button>
         </Link>
         <div className="flex">
           <AiOutlineShop className="h-5 w-5" /> Become a Seller
